e44fd4d8fa7db7b7517db40347270bad0889cc86
oussama-ababou <104654536+oussama-ababou@users.noreply.github.com>
2023-08-11 17:42:30 +0100

    11-08-lastupdates

--- bootstrapdemo/animatedloginform/src/LogRegComponent/LoginComp.jsx
+++ bootstrapdemo/animatedloginform/src/LogRegComponent/LoginComp.jsx
@@ -1,90 +1,5 @@
-// import React, { useState } from 'react';
-// import './logincomp.css';
-
-// const LoginComp = () => {
-//   const [isSignInActive, setIsSignInActive] = useState(true);
-
-//   const handleButtonClick = () => {
-//     const container = document.getElementById('container');
-//     container.classList.toggle('right-panel-active');
-//   };
-
-//   return (
-//     <div
-//       className={`container ${isSignInActive ? '' : 'right-panel-active'}`}
-//       id="container"
-//     >
-//       <div className="form-container sign-up-container">
-//         <form action="#">
-//           <h1>Create Account</h1>
-//           <div className="social-container">
-//             <a href="#" className="social">
-//               <i className="fab fa-facebook-f"></i>
-//             </a>
-//             <a href="#" className="social">
-//               <i className="fab fa-google-plus-g"></i>
-//             </a>
-//             <a href="#" className="social">
-//               <i className="fab fa-linkedin-in"></i>
-//             </a>
-//           </div>
-//           <span>or use your email for registration</span>
-//           <input type="text" placeholder="Name" />
-//           <input type="email" placeholder="Email" />
-//           <input type="password" placeholder="Password" />
-//           <button>Sign Up</button>
-//         </form>
-//       </div>
-//       <div className="form-container sign-in-container">
-//         <form action="#">
-//           <h1>Sign in</h1>
-//           <div className="social-container">
-//             <a href="#" className="social">
-//               <i className="fab fa-facebook-f"></i>
-//             </a>
-//             <a href="#" className="social">
-//               <i className="fab fa-google-plus-g"></i>
-//             </a>
-//             <a href="#" className="social">
-//               <i className="fab fa-linkedin-in"></i>
-//             </a>
-//           </div>
-//           <span>or use your account</span>
-//           <input type="email" placeholder="Email" />
-//           <input type="password" placeholder="Password" />
-//           <a href="#">Forgot your password?</a>
-//           <button>Sign In</button>
-//         </form>
-//       </div>
-//       <div className="overlay-container">
-//         <div className={`overlay ${isSignInActive ? '' : 'overlay-right'}`}>
-//           <div className="overlay-panel overlay-left">
-//             <h1>Welcome Back!</h1>
-//             <p>
-//               To keep connected with us please login with your personal info
-//             </p>
-//             <button className="ghost" onClick={handleButtonClick}>
-//               Sign In
-//             </button>
-//           </div>
-//           <div className="overlay-panel overlay-right">
-//             <h1>Hello, Friend!</h1>
-//             <p>Enter your personal details and start your journey with us</p>
-//             <button className="ghost" onClick={handleButtonClick}>
-//               Sign Up
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoginComp;
-// ... (import statements and other code above)
-
 import React, { useState } from 'react';
-import './logincomp.css'; // Import your CSS file
+import './logincomp.css';
 
 const LoginComp = () => {
   const [isSignInActive, setIsSignInActive] = useState(true);
@@ -99,21 +14,75 @@ const LoginComp = () => {
       className={`container ${isSignInActive ? '' : 'right-panel-active'}`}
       id="container"
     >
-      {/* Worker Sign Up Section */}
-      <div className={`form-container sign-up-container worker-background`}>
-        <form action="#">{/* Existing worker sign up form content */}</form>
+      <div className="form-container sign-up-container">
+        
+      <h1 className='h-client'>Client</h1>
+
+         
+          <button className="button-client">Sign Up</button>
+        
       </div>
 
-      {/* Client Sign Up Section */}
-      <div className={`form-container sign-up-container client-background`}>
-        <form action="#">{/* Existing client sign up form content */}</form>
-      </div>
+
+
+
+
+
+       
+      <div className="form-container2 sign-in-container">
+        <form action="#">
+          <h1>Sign in</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your account</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+       </div>
+
+
+
+
+
 
       <div className="overlay-container">
-        {/* Existing overlay and panel content */}
+               <div className={`overlay ${isSignInActive ? '' : 'overlay-left'}`}>
+          <div className="overlay-panel overlay-left">
+          <button className="close-button" onClick={handleButtonClick}>X</button>
+
+
+            <h1 className='h-ouvrier'>Ouvrier</h1>
+           
+            
+            <button className="button-ouvrier" >
+              Sign UP
+            </button>
+          </div>
+
+          <div className="overlay-panel2 overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start your journey with us</p>
+            <button className="ghost" onClick={handleButtonClick}>
+              Sign Up
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default LoginComp;
+
+
