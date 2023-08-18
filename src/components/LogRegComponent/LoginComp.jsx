3f51f0650cb43758e4b3b597670c2fc53a3d47de
j-tw1 <bouhmidiamine1@gmail.com>
2023-08-18 17:37:09 +0100

    18-08 small edit

--- src/components/LogRegComponent/LoginComp.jsx
+++ src/components/LogRegComponent/LoginComp.jsx
@@ -1,82 +1,135 @@
 import React, { useState } from 'react';
 import './logincomp.css';
+import { getRole, getToken, loginAPICall, saveLoggedInUser, storeRole, storeToken, storeTokenAndRole } from '../../services/AuthService';
+import { useNavigate } from 'react-router-dom';
 
 const LoginComp = () => {
+
+
+
+  const [username , setUsername] = useState('')
+
+  const [password , setPassword] = useState('')
+  
   const [isSignInActive, setIsSignInActive] = useState(true);
 
+  const navigator = useNavigate();
+
   const handleButtonClick = () => {
-    const container = document.getElementById('container');
+    const container = document.getElementById('container10');
     container.classList.toggle('right-panel-active');
-  };
+
+  }
+
+  async function handleloginClick (e) {
+    e.preventDefault();
+    
+   await loginAPICall(username,password).then((response)=>{
+      console.log(response.data);
+     
+      const token = 'Bearer ' + response.data.accessToken; 
+      const role =  response.data.role ;
+      
+      storeToken(token);
+      
+      storeRole(role);
+
+      saveLoggedInUser(username);
+
+      const roleY = getRole();
+
+      console.log(roleY);
   
+      navigator('/home');
+      
+    }) .catch(error=> {
+      console.error(error);
+    })
+    
+    
+  }
+
+
   return (
-    <div
-      className={`container ${isSignInActive ? '' : 'right-panel-active'}`}
-      id="container"
-    >
-      <div className="form-container sign-up-container">
-        <form action="#">
-          <h1>Create Account</h1>
-          <div className="social-container">
-            <a href="#" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-          <span>or use your email for registration</span>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-      <div className="form-container sign-in-container">
-        <form action="#">
-          <h1>Sign in</h1>
-          <div className="social-container">
-            <a href="#" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-          <span>or use your account</span>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <a href="#">Forgot your password?</a>
-          <button>Sign In</button>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className={`overlay ${isSignInActive ? '' : 'overlay-right'}`}>
-          <div className="overlay-panel overlay-left">
-            <h1>Welcome Back!</h1>
-            <p>
-              To keep connected with us please login with your personal info
-            </p>
-            <button className="ghost" onClick={handleButtonClick}>
-              Sign In
-            </button>
-          </div>
-          <div className="overlay-panel overlay-right">
-            <h1>Hello, Friend!</h1>
-            <p>Enter your personal details and start your journey with us</p>
-            <button className="ghost" onClick={handleButtonClick}>
-              Sign Up
-            </button>
+    <>
+      <div className="sign-ch">
+        <div className="sign-ch-two pb-70">
+          <div className="container"></div>
+          <div
+            className={`container1 ${
+              isSignInActive ? '' : 'right-panel-active'
+            }`}
+            id="container10"
+          >
+            <div className="form-container sign-up-container">
+              <h1 className="h-client">Client</h1>
+              <a href="/SignupClient">
+                <button className="button-client">Sign Up</button>
+              </a>
+            </div>
+
+            <div className="form-container2 sign-in-container">
+              <form action="#">
+                <h1>Sign in</h1>
+                <div className="social-container">
+                  <a href="#" className="social">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="#" className="social">
+                    <i className="fab fa-google-plus-g"></i>
+                  </a>
+                  <a href="#" className="social">
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                </div>
+                <span>or use your account</span>
+                <input type="text"
+
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}  // Corrected line
+                        placeholder="Email" />
+
+                <input type="password"
+                       placeholder="Password" 
+                       value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                       />
+                <a href="#">Forgot your password?</a>
+                <button className="button11" onClick={handleloginClick}>Sign In</button>
+              </form>
+            </div>
+
+            <div className="overlay-container">
+              <div
+                className={`overlay ${isSignInActive ? '' : 'overlay-left'}`}
+              >
+                <div className="overlay-panel overlay-left">
+                  <button className="close-button" onClick={handleButtonClick}>
+                    X
+                  </button>
+
+                  <h1 className="h-ouvrier">Ouvrier</h1>
+
+                  <a href="/Signup/ouvrier">
+                    <button className="button-ouvrier">Sign UP</button>
+                  </a>
+                </div>
+
+                <div className="overlay-panel2 overlay-right">
+                  <h1>Hello, Friend!</h1>
+                  <p>
+                    Enter your personal details and start your journey with us
+                  </p>
+                  <button className="ghost" onClick={handleButtonClick}>
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -14,54 +14,7 @@ function Feature() {
           <div className="banner-content text-center">
             <p>{t('SubTitleLogin')}</p>
             <h1>join us &amp; small text</h1>
-            <form className="banner-form">
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Keyword:</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      placeholder="Job Title"
-                    />
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div className="form-group">
-                    <label htmlFor="exampleInputEmail2">Location:</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="exampleInputEmail2"
-                      placeholder="City or State"
-                    />
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <button type="submit" className="find-btn">
-                    Find A Job
-                    <i className="bx bx-search" />
-                  </button>
-                </div>
-              </div>
-            </form>
-            <ul className="keyword">
-              <li>Trending Keywords:</li>
-              <li>
-                <a href="#">Automotive,</a>
-              </li>
-              <li>
-                <a href="#">Education,</a>
-              </li>
-              <li>
-                <a href="#">Health</a>
-              </li>
-              <li>and</li>
-              <li>
-                <a href="#">Care Engineering</a>
-              </li>
-            </ul>
+            
           </div>
         </div>
       </div>
